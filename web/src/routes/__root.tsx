6a6fe5517c02e4a34ece71bff25f0d2c9ebbcdde
FelipeMalacarne <felipemalacarne012@gmail.com>
2026-02-03 14:53:55 -0300

    New theme

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -2,7 +2,6 @@ import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { TanStackDevtools } from "@tanstack/react-devtools";
 
-import Header from "../components/header";
 import { AppSidebar } from "@/components/app-sidebar";
 import { ThemeProvider } from "@/providers/theme-provider";
 
